38bedda5241ee6c9dfaf846b822be23c2cc09b5f
Subhoshri Pal <138369802+Subhoshri@users.noreply.github.com>
2024-07-16 21:04:45 +0530

    Update InfoCard.jsx

--- src/components/InfoCards/InfoCard.jsx
+++ src/components/InfoCards/InfoCard.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./InfoCard.css";
-// import humidity from
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudShowersHeavy, faDroplet, faTemperatureThreeQuarters, faWind } from "@fortawesome/free-solid-svg-icons";
-import sunrise from "../../assets/sunrise.svg"
+import sunrise from "../../assets/sunrise.svg";
 
-var iconmap = {
+const iconmap = {
     "feels like": faTemperatureThreeQuarters,
     humidity: faDroplet,
     wind: faWind,
@@ -13,10 +12,12 @@ var iconmap = {
 };
 
 export const InfoCard = ({ field, value }) => {
+    const icon = iconmap[field.toLowerCase()];
+
     return (
         <div className="info-card">
             <div className="icon">
-                <FontAwesomeIcon icon={iconmap[field]} />
+                {icon ? <FontAwesomeIcon icon={icon} /> : <span>No icon</span>}
             </div>
             <div className="value">{value}</div>
             <div className="field">{field}</div>
@@ -24,12 +25,12 @@ export const InfoCard = ({ field, value }) => {
     );
 };
 
-export function TimeCard({ time, amORpm }) {
+export function TimeCard({ time, amORpm, label = "Sunrise" }) {
     return (
         <div className="time-card">
-            <div className="field">Sunrise</div>
+            <div className="field">{label}</div>
             <div className="time">
-                <img src={sunrise} alt="" srcSet="" className="sun-icon" />
+                <img src={sunrise} alt={label} className="sun-icon" />
                 <span>{time}</span>
                 <span style={{ fontSize: "1rem" }}> {amORpm}</span>
             </div>
